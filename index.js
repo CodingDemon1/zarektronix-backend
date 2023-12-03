53c6785c2ea44b5a0a418c60624972007d31df40
Nitish Kumar <kumar.nit1021@gmail.com>
2023-12-03 20:54:39 +0530

    Added relevent comments

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 
 const API_URI = "https://icanhazdadjoke.com/search?term";
 
+// Getting the SearchTerm from commandline.
+const args = process.argv[2];
+// <-------------------------->
+
+// Humorous Messages templates which will pass with the retrieved joke from the API .
 const humorousMessages = [
 	"Get ready to burst into laughter—joke incoming!",
 	"Hold on tight! Your laugh-inducing joke is on its way!",
@@ -15,7 +20,24 @@ const humorousMessages = [
 	"Your laughter prescription has been filled. Joke imminent!",
 	"Great news! A joke is on the horizon, and it's heading straight to you!",
 ];
+// <------------------------------->
 
+// Message templates if the user dont provide the keyword (searchterm).
+const funnyKeywordMissingMessages = [
+	"Hold up! We need a keyword to find the funny. Type in something hilarious!",
+	"No keyword, no jokes! Quick, type in a word and let the laughter begin!",
+	"Keyword needed: Our joke-finding ducks are quacking impatiently. What's the word?",
+	"Error 101: Keyword Missing! Please enter a funny keyword to unlock the humor vault.",
+	"Attention humor-seeker: The keyword slot is feeling lonely. Fill it up for instant laughs!",
+	"Knock, knock. Who's there? Oh wait, we need a keyword first! Type in something funny!",
+	"No keyword, no punchline! It's like trying to tell a joke without setup. Enter a keyword, please!",
+	"Houston, we have a problem. No keyword detected! Please provide a funny word or phrase.",
+	"Our joke engine is fueled by keywords. Without one, it's like trying to fly without wings. Enter a keyword, and let's soar!",
+	"Hold your horses! We're missing a keyword, and jokes can't gallop without it. Type something amusing!",
+];
+// <--------------------------------->
+
+// Message templates for error 404 (Joke not found from the API).
 const wittyNotFoundMessages = [
 	"Looks like the joke is on vacation! Try another keyword.",
 	"The joke gods are taking a day off. Maybe try something else?",
@@ -28,7 +50,9 @@ const wittyNotFoundMessages = [
 	"Joke not found. Maybe it's playing hide and seek. Try another keyword!",
 	"Looks like the joke's playing hooky. Try a different keyword and catch it later.",
 ];
+// <-------------------------------->
 
+// Message templates for empty leaderboard.
 const wittyLeaderboardNotFoundMessages = [
 	"The leaderboard seems to be on a humor diet. No jokes to display!",
 	"Looks like the jokes in the leaderboard are playing hide and seek. Try again later.",
@@ -41,16 +65,23 @@ const wittyLeaderboardNotFoundMessages = [
 	"Jokes on strike! The leaderboard is currently jokeless. Try another time.",
 	"The joke leaderboard is on vacation. No humor to report at the moment.",
 ];
-const args = process.argv[2];
+// <-------------------------->
 
-if (args.toLowerCase() === "leaderboard") {
+if (!args) {
+	console.log(
+		// Printing a random funny message if the keyword is missing
+		funnyKeywordMissingMessages[
+			Math.floor(Math.random() * funnyKeywordMissingMessages.length)
+		]
+	);
+} else if (args.toLowerCase() === "leaderboard") {
 	displayTheLeaderboard();
 } else {
 	findJokes(args);
 }
 
 function findJokes(keyword) {
-	console.log(`Searching a joke based on ${keyword}`);
+	console.log(`Searching a joke based on "${keyword}".`);
 
 	const options = {
 		url: `${API_URI}=${keyword}`,
@@ -58,8 +89,8 @@ function findJokes(keyword) {
 	};
 	var result = null;
 
+	// Fetching the joke from API
 	request(options, (err, res, body) => {
-		// console.log(res);
 		if (!err) {
 			result = JSON.parse(body).results;
 
@@ -69,13 +100,15 @@ function findJokes(keyword) {
 				console.log(
 					`${
 						humorousMessages[
-							Math.floor(Math.random() * humorousMessages.length)
+							Math.floor(Math.random() * humorousMessages.length) //printing a random humourous Message with the  retrieved joke from the API.
 						]
-					}\n\n${data.joke}
+					}\n\n----------------------------------------\n${
+						data.joke
+					}\n----------------------------------------
                     `
 				);
 
-				fs.appendFileSync("jokes.txt", `${data.joke}\n\n`);
+				fs.appendFileSync("jokes.txt", `${data.joke}\n\n`); // Adding the joke to the file "joke.txt"
 
 				setTimeout(() => {
 					console.log("Joke saved to jokes.txt for future laughs");
@@ -83,7 +116,7 @@ function findJokes(keyword) {
 			} else {
 				console.log(
 					wittyNotFoundMessages[
-						Math.floor(Math.random() * wittyNotFoundMessages.length)
+						Math.floor(Math.random() * wittyNotFoundMessages.length) // Printing a random Message from wittyNotFoundMessages array
 					]
 				);
 			}
@@ -97,6 +130,7 @@ function findJokes(keyword) {
 
 function displayTheLeaderboard() {
 	fs.readFile("jokes.txt", "utf8", (err, data) => {
+		//Reading the txt file
 		if (err) {
 			console.log(
 				"No jokes found in the leaderboard. Start saving some jokes!"
@@ -113,6 +147,8 @@ function displayTheLeaderboard() {
 
 				var leaderboardJokes = [];
 
+				//Creating the leaderboard.
+
 				for (let key in jokeObj) {
 					leaderboardJokes.push([key, jokeObj[key]]);
 				}
@@ -120,6 +156,7 @@ function displayTheLeaderboard() {
 
 				const len = Math.min(leaderboardJokes.length, 5);
 
+				// Printing the leaderboard.
 				console.log(
 					`The top ${len} searched jokes in the leaderboard are:\n--------------------------------------\n`
 				);
@@ -135,6 +172,7 @@ function displayTheLeaderboard() {
 				}
 				console.log(`\n\nKeep laughing!`);
 			} else {
+				// Printing a random Message from wittyLeaderboardNotFoundMessages array
 				console.log(
 					wittyLeaderboardNotFoundMessages[
 						Math.floor(Math.random() * wittyLeaderboardNotFoundMessages.length)
